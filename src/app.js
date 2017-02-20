bc86eb4dc7aaec7797f8fb505269080e9cdbe4ee
Diogo <diogo.c.frazao@ist.utl.pt>
2017-02-20 16:12:20 +0000

    Section 13
    Rendering Lists the Right Way

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
@@ -7,14 +8,14 @@ import { Header } from './components/common';
 import LibraryList from './components/LibraryList';
 
 const App = () => {
-    return (
-        <Provider store={createStore(reducers)}>
-            <View>
-                <Header headerText="Tech Stack" />
-                <LibraryList />
-            </View>
-        </Provider>
-    );
+  return (
+    <Provider store={createStore(reducers)}>
+      <View syle={{ flex: 1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
 };
 
 export default App;
